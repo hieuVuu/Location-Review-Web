0538d34e73a1cf6b5790eccb090b1166b830813f
Devil-VU <vuchihieuu112@gmail.com>
2020-11-05 12:32:13 +0700

    redirect-login-register

--- js/views.js
+++ js/views.js
@@ -3,9 +3,15 @@ view.setActiveScreen = (screenName) => {
        switch(screenName) {
               case 'loginScreen' :
               document.getElementById('app').innerHTML = components.loginScreen 
+              document.getElementById('redirect-register').addEventListener('click',() => {
+                     view.setActiveScreen('registerScreen')
+              })
               break;
               case 'registerScreen' : 
               document.getElementById('app').innerHTML = components.registerScreen
+              document.getElementById('redirect-login').addEventListener('click',() => {
+                     view.setActiveScreen('loginScreen')
+              })
               break;
        }
       
